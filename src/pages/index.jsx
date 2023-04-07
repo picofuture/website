00305d39b4460cef1a07ef11f66fd2d7d779b772
Anfal <muhammadanfal0@gmail.com>
2023-04-07 09:01:59 -0700

    WIP

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -245,7 +245,7 @@ function Resume() {
         ))}
       </ol>
       <Button href="/api/resume" variant="secondary" className="group mt-6 w-full">
-        Download CV
+        Download Complete CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
