bdad0cbddc02432f2887af7550229eaf966604dc
Anfal <muhammadanfal0@gmail.com>
2023-04-17 20:43:34 -0700

    Complete thread creation.

--- src/actions/create_twitter_thread.js
+++ src/actions/create_twitter_thread.js
@@ -1,6 +1,5 @@
-import {getAllArticles} from "../lib/getAllArticles.js";
-import {supabase} from "../lib/supabaseClient.js";
-import {TWITTER_POSTS} from "../lib/sharedConsts.js";
+const createClient = require('@supabase/supabase-js').createClient;
+const { Configuration, OpenAIApi } = require("openai");
 
 // This is here for testing purposes only.
 // On local, we need to load the environment variables. Because I won't be using next js.
@@ -9,15 +8,74 @@ if (process.env.NODE_ENV === 'develop') {
   require('../lib/loadEnv');
 }
 
+const SOCIAL_MEDIA_BLOG_POSTS = 'social_media_blog_posts';
+const DELIMITER = '[,]';
+
+const getSupabaseClient = () => {
+  return createClient(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
+  );
+}
+
+const getOpenAIClient = () => {
+  const configuration = new Configuration({
+    organization: process.env.OPEN_AI_ORGANIZATION,
+    apiKey: process.env.OPEN_AI_API_KEY,
+  });
+
+  return new OpenAIApi(configuration);
+}
+
+const getArticleToProcess = async () => {
+  const selectResponse = await getSupabaseClient()
+    .from(SOCIAL_MEDIA_BLOG_POSTS)
+    .select('*')
+    .eq('is_posted', 0);
+
+  if (selectResponse.error || selectResponse.status !== 200) {
+    return null;
+  }
+
+  return selectResponse.data[0];
+}
+
+const updateArticleInDB = async (article, formattedContent) => {
+  const updateResponse = await getSupabaseClient()
+    .from(SOCIAL_MEDIA_BLOG_POSTS)
+    .update({ formatted_content: formattedContent })
+    .eq('id', article.id);
+
+  return !(updateResponse.error || updateResponse.status !== 200);
+}
+
 const generatePrompt = (articleName, content) => {
   return `
-  Create an engaging and detailed Twitter thread based on the blog content with the topic "${articleName}" given below. 
-  Make sure to include relevant hashtags.
-  
-  Context: ${content}
-  
-  
+  Create an engaging and detailed Twitter thread based on the blog content with the topic "${articleName}" given below.\n
+  Make sure to include relevant hashtags.\n
+  Separate each tweet with a delimiter ${DELIMITER}.\n
+  Convert markdown into regular text.\n
+  Context: ${content}\n\n
   `;
+}
+
+const generateThread = async (prompt) => {
+  const response = await getOpenAIClient().createChatCompletion({
+    model: "gpt-4",
+    messages: [
+      {
+        role: "system",
+        content: "You are a Twitter bot who creates Twitter threads based on the given blog content. Make sure length of each thread does not exceed 280 characters."
+      },
+      {
+        role: "user",
+        content: prompt
+      }
+    ],
+    temperature: 1,
+  });
+
+  return response.data.choices[0].message.content;
 }
 
 const addAdvertisementToArray = (threadArray, article) => {
@@ -26,29 +84,32 @@ const addAdvertisementToArray = (threadArray, article) => {
   return threadArray;
 }
 
-const getFilteredArticles = async (existingArticlesArray) => {
-  const allArticles = await getAllArticles();
+const getFormattedThreadArray = (gptResponse, article) => {
+  let localResponse = gptResponse;
 
-  return allArticles.filter(article => {
-    return !existingArticlesArray.includes(article.slug);
-  });
-}
+  localResponse = localResponse.split(DELIMITER);
 
-const getStoredArticles = async () => {
-  const selectResponse = await supabase
-    .from(TWITTER_POSTS)
-    .select('*');
+  localResponse = localResponse.map((tweet) => tweet.startsWith('\n\n') ? tweet.slice(2) : tweet)
 
-  if (selectResponse.error || selectResponse.status !== 200) {
-    return null
-  }
+  localResponse = addAdvertisementToArray(localResponse, article);
 
-  return selectResponse.data;
+  return localResponse;
 }
 
 const main = async () => {
-  const storedArticles = await getStoredArticles();
-  console.log('storedArticles', storedArticles);
+  console.log('Starting the Twitter Bot');
+
+  const article = await getArticleToProcess();
+
+  const formattedContent = generatePrompt(article.slug, article.raw_content);
+
+  let formattedResponse = await generateThread(formattedContent);
+
+  const formattedThreadArray = getFormattedThreadArray(formattedResponse, article);
+
+  const updateResult = await updateArticleInDB(article, formattedThreadArray);
+
+  console.log('Updated article in DB ', updateResult ? 'successfully' : 'unsuccessfully');
 }
 
-await main();
+main();
