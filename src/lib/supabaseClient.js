72447e61fde3c3627c518a36f943be0acc857d50
Anfal <muhammadanfal0@gmail.com>
2025-03-07 16:52:43 -0800

    Print the key

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  `${process.env.SUPABASE_URL}`,
-  `${process.env.SUPABASE_ANON_KEY}`
-)
+export const supabase = () => {
+  console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+  return createClient(
+    `${process.env.SUPABASE_URL}`,
+    `${process.env.SUPABASE_ANON_KEY}`
+  );
+}
