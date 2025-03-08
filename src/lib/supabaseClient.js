9df2de90148956049c671efc37d2ee7bdd32610a
Anfal <muhammadanfal0@gmail.com>
2025-03-07 16:59:04 -0800

    Remove console log

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const getSupabaseClient = () => {
-  console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
   return createClient(
     `${process.env.SUPABASE_URL}`,
     `${process.env.SUPABASE_ANON_KEY}`
