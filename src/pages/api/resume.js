108ad5d2f1b6b1bdeb2fb276b876d973e7ade79e
Anfal <muhammadanfal0@gmail.com>
2025-09-30 11:34:40 -0700

    Update resume

--- src/pages/api/resume.js
+++ src/pages/api/resume.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 import {formatCDNURl} from "@/lib/cloudflareImageLoader";
 
 export default async function handler(req, res) {
-  const resumeName = '04_07_2025_resume_anfal.pdf';
+  const resumeName = '2025_09_30_resume_anfal.pdf';
   const fileUrl = formatCDNURl(resumeName, 'docs');
 
   // Fetch the PDF file from the CDN
